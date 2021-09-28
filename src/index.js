d18e6b5bef86591e4e9486d15bf3f68702cb6461
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-09-28 17:58:11 +0200

    Comenzada primera seccion del main

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import "./styles.css";
 import "./styles.scss";
+import imgSrc1 from "./static/img1.png";
 
-//?Menu Desplegable
+//? Menu Desplegable
 
 const iconoMenu = document.querySelector(".nav__icono-menu");
 const menuDesplegable = document.querySelector(".menu-desplegable");
+
 iconoMenu.addEventListener("click", () => {
 	iconoMenu.classList.toggle("fa-bars");
 	iconoMenu.classList.toggle("fa-times");
-	menuDesplegable.classList.toggle("opacity");
+	menuDesplegable.classList.toggle("scale");
 });
+
+//? Imagenes
+let img1 = document.querySelector(".main__img1");
+img1.src = imgSrc1;
