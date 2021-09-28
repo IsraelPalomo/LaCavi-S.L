d437b235d7fe5f9ec985d2819087d58aa109207c
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-09-28 16:03:30 +0200

    Menu desplegable acabado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "./styles.css";
 import "./styles.scss";
+
+//?Menu Desplegable
+
+const iconoMenu = document.querySelector(".nav__icono-menu");
+const menuDesplegable = document.querySelector(".menu-desplegable");
+iconoMenu.addEventListener("click", () => {
+	iconoMenu.classList.toggle("fa-bars");
+	iconoMenu.classList.toggle("fa-times");
+	menuDesplegable.classList.toggle("opacity");
+});
