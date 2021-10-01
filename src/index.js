a653fe41641c0860b1521c5cf2c18fcf60f4109f
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-10-01 05:49:12 +0200

    seccion servicios terminada

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 import "./styles.scss";
+import imgSrc1 from "./static/presupuesto.png";
+import imgSrc2 from "./static/obrero.png";
+import imgSrc3 from "./static/virus.png";
 
 //? Menu Desplegable
 
@@ -13,3 +16,10 @@ iconoMenu.addEventListener("click", () => {
 });
 
 //? Imagenes
+
+const img1 = document.querySelector(".main__servicios--presupuestos");
+img1.src = imgSrc1;
+const img2 = document.querySelector(".main__servicios--mano-obra");
+img2.src = imgSrc2;
+const img3 = document.querySelector(".main__servicios--seguridad");
+img3.src = imgSrc3;
